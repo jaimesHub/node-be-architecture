13199c488f134e3a56611e9d6d8351e70fc821e1
JaImes <duongquockhanh230596@gmail.com>
2026-08-24 15:37:08 +0700

    [Practice] level 0

---
+++ practice/src/configs/config.mongodb.js
@@ -0,0 +1,15 @@
+'use strict'
+
+// level 0
+const config = {
+  app: {
+    port: 3000,
+  },
+  db: {
+    host: '127.0.0.1',
+    port: 27017,
+    name: 'db,'
+  },
+};
+
+module.exports = config;
