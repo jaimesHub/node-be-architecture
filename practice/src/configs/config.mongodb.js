97a2b021affe627db6f19f320e6e327cdea5c455
JaImes <duongquockhanh230596@gmail.com>
2026-08-24 15:57:47 +0700

    [Practice] level 1 - update process.env

--- practice/src/configs/config.mongodb.js
+++ practice/src/configs/config.mongodb.js
@@ -16,23 +16,23 @@
 
 const dev = {
   app: {
-    port: 3000,
+    port: process.env.DEV_APP_PORT || 3000,
   },
   db: {
-    host: '127.0.0.1',
-    port: 27017,
-    name: 'dbDev,'
+    host: process.env.DEV_DB_HOST || '127.0.0.1',
+    port: process.env.DEV_DB_PORT || 27017,
+    name: process.env.DEV_DB_NAME || 'dbDev,'
   },
 };
 
 const prod = {
   app: {
-    port: 3000,
+    port: process.env.PROD_APP_PORT || 3000,
   },
   db: {
-    host: '127.0.0.1',
-    port: 27017,
-    name: 'dbProd'
+    host: process.env.PROD_DB_HOST || '127.0.0.1',
+    port: process.env.PROD_DB_PORT || 27017,
+    name: process.env.PROD_DB_NAME || 'dbProd'
   },
 };
 
